d2f980a64f4ce4de552359d6c9eb478ab5c3697a
Ali EL ABBASSY <aea@Alis-MacBook-Pro.local>
2021-11-23 13:16:50 +0100

    fix: preprocessing -> added target as parameter to make it  work for both blackbox and surrogate models

--- models/blackbox/preprocessing.py
+++ models/blackbox/preprocessing.py
@@ -5,23 +5,8 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 
-def get_preprocessed_data():
+def get_preprocessed_data(target):
     df = pd.read_excel("../../data/data_project.xlsx")
-
-    features = [
-        "CreditDuration",
-        "CreditAmount",
-        "InstallmentRate",
-        "Age",
-        "NumberOfCredits",
-        "CreditHistory",
-        "EmploymentDuration",
-        "Housing",
-        "Purpose",
-        "Savings",
-        "Group",
-        "Gender",
-    ]
 
     numerical_features = [
         "CreditDuration",
@@ -40,15 +25,20 @@ def get_preprocessed_data():
         "Group",
         "Gender",
     ]
+    
+    features = numerical_features + categorical_features
 
-    target = "CreditRisk (y)"
+    #target = target
 
     # assert features == categorical_features + numerical_features
+    
+    df = df.dropna(subset=[target])
 
     X, y = df[features], df[target]
 
     # Label-encoding categorical variables
     for cat_feature in categorical_features:
+        X[cat_feature].fillna("Unknown", inplace=True)
         encoder = LabelEncoder()
         X[cat_feature] = encoder.fit_transform(X[cat_feature])
 
