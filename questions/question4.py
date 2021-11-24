c3aab16c8a22f956bb4764761640a80dd84d680b
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2021-11-24 12:56:41 +0100

    question 4

---
+++ questions/question4.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from algo_fairness.surrogate.train_lr_surrogate import train_lr_surrogate
+from algo_fairness.blackbox.preprocessing import get_preprocessed_data
+
+X = get_preprocessed_data("../data/data_project.xlsx", "CreditRisk (y)")[0]
+
+with open("../outputs/proba_lgb_blackbox.npx", "rb") as infile:
+    y_hat = np.load(infile)
+
+train_lr_surrogate(X, y_hat)
