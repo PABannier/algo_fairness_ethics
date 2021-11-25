90ed51b4c7023e67d62f7086f802dd014f33243a
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2021-11-25 15:55:58 +0100

    refactored code to package

---
+++ algo_fairness/tools/pdp.py
@@ -0,0 +1,41 @@
+from sklearn.inspection import PartialDependenceDisplay
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = 16, 9
+
+
+def pdp_ice(
+    model,
+    X_train,
+    features,
+    kind="both",
+    subsample=50,
+    n_jobs=3,
+    grid_resolution=20,
+    random_state=0,
+):
+    """
+    Plots the partial dependence, both individual (ICE) and averaged one (PDP)
+    Args:
+        model : Trained model
+        X_train : X_train used to train the model
+        features (list): list of the fetaures
+        kind {‘average’, ‘individual’, ‘both’} :
+            'average' results in the traditional PD plot
+            'individual' results in the ICE plot
+            'both' results in the overlay of both
+        subsample (int) : number of ICE curves to display
+    """
+    display = PartialDependenceDisplay.from_estimator(
+        model,
+        X_train,
+        features,
+        kind=kind,
+        subsample=subsample,
+        n_jobs=n_jobs,
+        grid_resolution=grid_resolution,
+        random_state=random_state,
+        ice_lines_kw={"color": "tab:blue", "alpha": 0.2, "linewidth": 0.5},
+        pd_line_kw={"color": "tab:orange", "linestyle": "--"},
+    )
+    display.figure_.subplots_adjust(hspace=0.5)
