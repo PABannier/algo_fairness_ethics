852f2486bc3136e9f3dec7ae46e28e24e46ff556
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2021-11-25 16:25:04 +0100

    updated questions 2, 3 and 5

--- questions/question2and3.py
+++ questions/question2and3.py
@@ -2,7 +2,7 @@ import joblib
 
 import numpy as np
 
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, train_test_split
 from sklearn.metrics import f1_score, roc_auc_score
 
 from algo_fairness.blackbox.preprocessing import get_preprocessed_data
@@ -19,14 +19,14 @@ X, y, _, categorical_features = get_preprocessed_data(
     "../data/data_project.xlsx", "CreditRisk (y)"
 )
 
-# Fitting
+# OOF model validation
 y_oof = np.zeros(X.shape[0])
 
 for i, (train_idx, valid_idx) in enumerate(kf.split(X, y)):
     train_data = X.iloc[train_idx, :], y[train_idx]
     valid_data = X.iloc[valid_idx, :], y[valid_idx]
 
-    model, y_pred_valid, log = fit_lgbm(
+    model, y_pred_valid = fit_lgbm(
         train_data,
         valid_data,
         cat_features=categorical_features,
@@ -36,12 +36,26 @@ for i, (train_idx, valid_idx) in enumerate(kf.split(X, y)):
     )
     y_oof[valid_idx] = y_pred_valid
 
-    with open(f"../outputs/lgbm_bb_fold_{i}.pkl", "wb") as outfile:
-        joblib.dump(model, outfile)
-
 print("OOF AUC score: ", roc_auc_score(y, y_oof))
 print("OOF F1 score:", f1_score(y, y_oof > 0.5))
 
 with open(OUT_PATH, "wb") as outfile:
     np.save(outfile, y_oof)
 print("Proba vector saved!")
+
+# Train-test-split with models
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=0
+)
+model, y_pred = fit_lgbm(
+    (X_train, y_train),
+    (X_test, y_test),
+    cat_features=categorical_features,
+    num_rounds=1000,
+    lr=0.05,
+    bf=0.7,
+)
+
+with open(f"../outputs/lgb_bb_model.pkl", "wb") as outfile:
+    joblib.dump(model, outfile)
--- questions/question5.py
+++ questions/question5.py
@@ -1,4 +1,23 @@
 import joblib
 
+from sklearn.model_selection import train_test_split
+
 from algo_fairness.blackbox.preprocessing import get_preprocessed_data
 from algo_fairness.tools.pdp import pdp_ice
+
+X, y, numerical_features, categorical_features = get_preprocessed_data(
+    "../data/data_project.xlsx", "CreditRisk (y)"
+)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=0
+)
+
+with open("../outputs/lgb_bb_model.pkl", "rb") as infile:
+    model = joblib.load(infile)
+
+pdp_ice(
+    model,
+    X_train,
+    numerical_features + categorical_features,
+)
